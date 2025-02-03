1093cb1582ea0b83d7ac5448e15a32375eebd13c
matti de beer <mattidebeer@gmail.com>
2025-02-03 15:10:39 +0000

    Updated training hyperparameters

--- scripts/train.py
+++ scripts/train.py
@@ -12,11 +12,11 @@ from trading_models.DQN_Model import train_dqn
 def main():
 
     # 2) Create environment
-    env = DQNTradingEnv(20)
+    env = DQNTradingEnv(10)
 
     # 3) Train agent
     # If using stable-baselines3 DQN:
-    model = train_dqn(env, total_timesteps=100000, verbose=1)
+    model = train_dqn(env, total_timesteps=500000, verbose=1)
 
     # 4) Evaluate
     obs, info = env.reset()
@@ -27,27 +27,44 @@ def main():
     action_arr = []
     price_arr = []
     step_arr = []
+    step = 0
     
     while not done:
         
         action, _ = model.predict(obs, deterministic=True)
         obs, reward, done, _ , info = env.step(action)
         total_reward += reward
-        action_arr.append(info['action'])
-        price_arr.append(info['price'])
-        step_arr.append(info['step'])
+        action_arr.append(info['action_type'])
+        price_arr.append(obs[-2])
+        step+= 1
+        step_arr.append(step)
         
     
     print("Test episode total reward:", total_reward)
     
     #5) Plot
-    price_arr = np.array(price_arr)
+    # Plot price vs step
     action_arr = np.array(action_arr)
-    buy_array_mask = action_arr == 1
-    sell_array_mask = action_arr == -1
-    plt.plot(step_arr, price_arr)   
-    plt.scatter(step_arr[buy_array_mask],price_arr[buy_array_mask], 'O')
-    plt.scatter(step_arr[sell_array_mask],price_arr[sell_array_mask], 'X')
+    step_arr = np.array(step_arr)
+    price_arr = np.array(price_arr)
+    plt.plot(step_arr, price_arr, label='Price')
+    
+    # Mark buy actions with triangles (1 in action_arr)
+    buy_steps = step_arr[action_arr == 1]
+    buy_prices = price_arr[action_arr == 1]
+    plt.scatter(buy_steps, buy_prices, marker='^', color='green', label='Buy', zorder=5)
+    
+    # Mark sell actions with circles (-1 in action_arr)
+    sell_steps = step_arr[action_arr == -1]
+    sell_prices = price_arr[action_arr == -1]
+    plt.scatter(sell_steps, sell_prices, marker='o', color='red', label='Sell', zorder=5)
+    
+    # Add labels and legend
+    plt.xlabel('Step')
+    plt.ylabel('Price')
+    plt.legend()
+    
+    # Show plot
     plt.show()
     
     
