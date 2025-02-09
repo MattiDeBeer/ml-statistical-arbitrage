b2777781dfddc52d525ca38d190e501fc80f28e8
matti de beer <mattidebeer@gmail.com>
2025-02-09 15:15:17 +0000

    Added a trading enviroment that uses a dictionary state space

--- envs/rl_enviroments.py
+++ envs/rl_enviroments.py
@@ -7,7 +7,7 @@ import numpy as np
 sys.path.append(os.path.abspath("../"))
 from envs.binance_trading_enviroment import BinanceTradingEnv
 
-class RlTradingEnv(BinanceTradingEnv,gymnasium.Env):
+class RlTradingEnvContinious(BinanceTradingEnv,gymnasium.Env):
     """Custom Trading Environment following Gymnasium interface"""
 
     def __init__(self, window_length = 10, dataset_length = 100):
@@ -19,7 +19,6 @@ class RlTradingEnv(BinanceTradingEnv,gymnasium.Env):
         
         #get complex sin wave dataset
         self.get_complex_sin_wave_dataset(dataset_length)
-        
         
         # Define action and observation space
         self.action_space = spaces.Discrete(2)  # 0 = Hold, 1 = Buy/Sell
@@ -134,7 +133,132 @@ class RlTradingEnv(BinanceTradingEnv,gymnasium.Env):
         info = {}
         
         return self.state, reward, done, truncated, info
+
+class RlTradingEnvDict(BinanceTradingEnv,gymnasium.Env):
+    """Custom Trading Environment following Gymnasium interface"""
+
+    def __init__(self, window_length = 10, dataset_length = 100):
+        super().__init__()
+    
+        # Load dataset form inherited trading env
+        #self.get_sin_wave_dataset(dataset_length, period=0.1, bin_size=10)
+        self.dataset_length = dataset_length
         
+        #get complex sin wave dataset
+        self.get_complex_sin_wave_dataset(dataset_length)
+        
+        # Define action and observation space
+        self.action_space = spaces.Discrete(2)  # 0 = Hold, 1 = Buy/Sell
+        n = window_length
+        
+        
+        self.observation_space = spaces.Dict({
+                'open': spaces.Box(low=-np.inf, high=np.inf, shape=(n,)),
+                #'close': spaces.Box(low=-np.inf, high=np.inf, shape=(n,)),
+                'previous_action' : spaces.Discrete(2),
+                'is_bought' : spaces.Discrete(2),
+                })
+
+        #define data window length
+        self.window_length = window_length
+        
+        # step forward to allow for a sufficient historical dataset window
+        self.step_time(window_length)
+        
+        self.token = 'SIN'
+        
+        #set current funds and transaction fees
+        self.money = 50
+        self.transaction_percentage = 0.01
+        
+        #set previous portfolio value
+        self.previous_value = 50
+        
+        # Setup action decay
+        self.action_decay_constant = np.log(2) / 10
+        self.action_decay_premultiplier = 0
+        self.steps_since_action = 0
+        
+        # Initialize state
+        self.is_bought = 0
+        self.state = self.generate_observation(0)
+        self.episode_length = dataset_length - window_length - 1
+        self.current_step = 0
+        
+    def reward_function(self, current_value, previous_value):
+        
+        #calculate reward
+        reward = np.log(current_value / previous_value)
+        
+        #ensure reward cannot be too large
+        if reward > 100:
+            reward = 100
+        elif reward < -100:
+            reward = 100
+        
+        return reward
+
+    def generate_observation(self,action):
+        state = {}
+        
+        current_data = self.get_historical_prices(self.token, self.window_length, return_data=True)[self.token]
+        
+        state['open'] = current_data['open']
+        #state['close'] = current_data['close']
+        state['previous_action'] = action
+        state['is_bought'] = self.is_bought
+        
+        return state
+        
+    def reset(self, seed=None, options=None):
+        """Reset the environment to the initial state"""
+        super().reset(seed=seed) 
+        self.close_all_positons()
+        self.is_bought = 0
+        self.time = self.window_length + 1
+        self.get_complex_sin_wave_dataset(self.dataset_length)
+        self.state = self.generate_observation(0)
+        self.current_step = 0
+        self.money = 20
+        self.previous_value = 20
+        return self.state, {}
+
+    def step(self, action):
+            
+        # Apply action
+        if action == 0:
+            self.current_value = self.get_current_portfolio_value()
+            reward = self.reward_function(self.current_value, self.previous_value)
+            self.step_time(1)
+            
+        else: 
+            # Sell if token is held
+            if self.is_bought == 1:
+                self.is_bought = 0
+                self.close_all_positons()
+                self.current_value = self.get_current_portfolio_value()
+                reward = self.reward_function(self.current_value, self.previous_value)
+                self.step_time(1)
+                
+            else:
+                # Buy if token is not held
+                self.is_bought = 1
+                self.buy_token(self.token, 1)
+                self.current_value = self.get_current_portfolio_value()
+                reward = self.reward_function(self.current_value,self.previous_value)
+                self.step_time(1)    
+                
+        self.current_step += 1
+        self.state = self.generate_observation(action)
+        self.previous_value = self.current_value
+        
+        # Check if episode is done
+        done = self.current_step >= self.episode_length
+        truncated = False  
+        
+        info = {}
+        
+        return self.state, reward, done, truncated, info
 
 class TestEnv(gymnasium.Env):
     def __init__(self):
