b312a3c060a9179a0be777884f84ba87378eb0c4
matti de beer <mattidebeer@gmail.com>
2025-02-09 15:18:57 +0000

    Moved the plotting call to its own cell

--- scripts/stable_baseline_train.py
+++ scripts/stable_baseline_train.py
@@ -5,12 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sys.path.append("../")
-from rl_models.stable_baseline_DQN_Model import DqnModel
-from envs.rl_enviroments import RlTradingEnv, TestEnv
+from rl_models.stable_baseline_DQN_Model import DqnModelCont, DqnModelDict
+from envs.rl_enviroments import RlTradingEnvContinious, RlTradingEnvDict, TestEnv
 
 
-Model = DqnModel(RlTradingEnv)
 
-Model.train(100000)
+Model = DqnModelDict(RlTradingEnvDict)
 
+Model.train(10000)
+
+#%%
 Model.plot_episode()
