b2e1c2526e9a448e121d1412ee0faaf77435f442
matti de beer <mattidebeer@gmail.com>
2025-03-12 21:12:43 +0000

    Added a config argument and example usage to allow for more customizaiton

--- scripts/stable_baseline_train.py
+++ scripts/stable_baseline_train.py
@@ -1,16 +1,31 @@
 # scripts/train.py
 
-import sys
+from models.stable_baseline_DQN_Model import DqnModel
+from envs.rl_enviroments import RlTradingEnvToken, RlTradingEnvSin
+import numpy as np
 
-sys.path.append("../")
-from rl_models.stable_baseline_DQN_Model import DqnModelCont, DqnModelDict
-from envs.rl_enviroments import RlTradingEnvContinious, RlTradingEnvDict, TestEnv, RlTradingEnvBTC
+config = {
+    
+    ### Enviroment Config ###
+    "enviromentClass": RlTradingEnvSin,
+    "episode_length": 500,
+    "token" : 'BTCUSDT',
+    "continious_dim" : 20,
+    "continious_obs" : {'open' : (-np.inf,np.inf), 'close' : (-np.inf, np.inf)},
+    "discrete_obs" : {'is_bought' : 2, 'previous_action' : 2},
+    "verbose" : True,
+    
+    ### Feature Extractor Config ###
+    "combiner_hidden" : [10,20],
+    "disc_hidden" : [3],
+    "lstm_hidden_size" : 30,
+    "disc_out_dim" : 5,
+    "compile_flag" : False
 
+}
 
-
-Model = DqnModelDict(RlTradingEnvBTC)
+Model = DqnModel(config)
 
 Model.train(10000)
 
-#%%
 Model.plot_episode()
