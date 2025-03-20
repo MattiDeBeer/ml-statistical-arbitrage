c439d03d342f2056b1e44e6fb6edcc03ea59d460
matti de beer <mattidebeer@gmail.com>
2025-03-20 18:48:27 +0000

    Added a training loop for the full training sequence

--- scripts/stable_baseline_train.py
+++ scripts/stable_baseline_train.py
@@ -42,14 +42,14 @@ single_config = {
 
 pairs_config = {
     
-    "run_id" : "0001",
+    "run_id" : "0005",
 
     ### Enviroment Config ###
     "enviromentClass": RlTradingEnvPairs,
     "episode_length": 800,
-    "timeseries_obs" : {},
+    "timeseries_obs" : {'z_score' : (10,-np.inf,np.inf)},
     "discrete_obs" : {'is_bought' : 2, 'previous_action' : 2},
-    "indicator_obs" : {'adfuller' : (0,1), 'coint_p_value' : (0,1), 'z_score' : (-np.inf,np.inf)},
+    "indicator_obs" : {'adfuller' : (0,1), 'coint_p_value' : (0,1)},
     "verbose" : False,
     "transaction_percentage" : 0,
     "token_pair" : ("BTCUSDT","ETHUSDT"),
@@ -58,6 +58,7 @@ pairs_config = {
     "log" : True,
     "dataset_file": "data/processed_dataset_5000_1h_train.h5",
     "test_dataset": "data/processed_dataset_5000_1h_test.h5",
+    "algo_env" : RlTradingEnvPairs,
     "pretrain_env" : RlPretrainEnvSingleAction,
     
     ### Feature Extractor Config ###
@@ -80,7 +81,7 @@ pairs_config = {
     "exploration_initial_eps": 1.0,
     "exploration_final_eps": 0.05,
     "exploration_fraction": 0.7,
-    "q_net_layers" : [64,64],
+    "q_net_layers" : [64,64,64],
     "verbose_level" : 0,
     "tensorboard_log_file" : "tensorboard_log",
     "model_save_folder" : "saved_models"
@@ -120,14 +121,14 @@ pairs_config_extended_actions = {
     ### DQN Config ###
     "learning_rate" : 1e-3,
     "buffer_size" : 1000,
-    "learning_starts": 50,
-    "batch_size": 32,
+    "learning_starts": 200,
+    "batch_size": 100,
     "gamma": 0.99,
     "target_update_interval": 500,
-    "exploration_initial_eps": 1.0,
+    "exploration_initial_eps": 0.25,
     "exploration_final_eps": 0.05,
     "exploration_fraction": 0.8,
-    "q_net_layers" : [32,32],
+    "q_net_layers" : [64,64,64],
     "verbose_level" : 0,
     "tensorboard_log_file" : "./tensorboard_log",
     "model_save_folder" : "saved_models"
@@ -141,7 +142,7 @@ pairs_config_extended_actions = {
 
 #################################################################
 
-### THIS TRAINS AND PLOTS AN EPISODE FOR PAIRS TRADING ###y
+### THIS TRAINS AND PLOTS AN EPISODE FOR PAIRS TRADING ###
 
 #Model = PairsDqnModel(pairs_config)
 #Model.train(1,eval_frequency=5,eval_steps=5)
@@ -154,15 +155,26 @@ pairs_config_extended_actions = {
 ### THIS TRAINS AND PLOTS AN EPISODE FOR PAIRS TRADING WITH EXTENDED ACTIONS ###
 
 Model = PairsDqnModel(pairs_config)
-Model.pre_train(2000,eval_frequency=10,eval_steps=5)
-Model.plot_pretrain_episode()
-#Model.pretrain_eval_episode(10,verbose=True)
-#Model.train(10)
-    
+Model.force_pretrain_qnet(dataset_size=10000,steps=1000,verbose_freq = 100)
+while Model.pretrain_eval_episode(30) <= 0:
+    Model.pre_train(200,eval_frequency=10,eval_steps=5)
 
-#Model.train_algo(1000,eval_frequency=10,eval_steps=5)
-#Model.train(1000,eval_frequency=10,eval_steps=5)
-#Model.plot_episode(action_num=2)
+average = Model.pretrain_eval_episode(30)
+print(f"Model Pretraining complete with an average reward of {average}")
+
+while Model.eval_episode(30,algo=True) <= 0:
+    Model.train_algo(200,eval_frequency=100,eval_steps=5)
+
+average = Model.eval_episode(30,algo=True)
+print(f"Model Pretraining complete with an average reward of {average}")
+
+while Model.eval_episode(30,algo=True) <= 0:
+    Model.train(200,eval_frequency=100,eval_steps=5)
+
+average = Model.eval_episode(30,algo=True)
+print(f"Model Pretraining complete with an average reward of {average}")
+
+Model.save(pairs_config['run_id_final'])
 #Model.save("test")
 #
 
